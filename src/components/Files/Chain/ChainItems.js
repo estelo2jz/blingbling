b889bf451e5b363a8a1edb78fe886ae0c50b4fde
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-11-02 13:53:32 -0600

    added title and images

---
+++ src/components/Files/Chain/ChainItems.js
@@ -0,0 +1,76 @@
+export const ChainItems = [
+  {
+    title: "JDN090517 - DIAMOND STAR CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain1/chain1.jpg'),
+    imageTwo: require('./chainImg/chain1/chain2.jpg')
+  },
+  {
+    title: "JDN1611291 - DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain2/chain1.jpg'),
+    imageTwo: require('./chainImg/chain2/chain2.jpg')
+  },
+  {
+    title: "JDN170119 - DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain3/chain1.jpg'),
+    imageTwo: require('./chainImg/chain3/chain2.jpg')
+  },
+  {
+    title: "JDN1701148 - BLACK DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain4/chain1.jpg'),
+    imageTwo: require('./chainImg/chain4/chain2.jpg')
+  },
+  {
+    title: "JDN40407 - DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain5/chain1.jpg'),
+    imageTwo: require('./chainImg/chain5/chain2.jpg')
+  },
+  {
+    title: "JDN6897 - BLUE DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain6/chain1.jpg'),
+    imageTwo: require('./chainImg/chain6/chain2.jpg')
+  },
+  {
+    title: "JDN8721 - DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain7/chain1.jpg'),
+    imageTwo: require('./chainImg/chain7/chain2.jpg')
+  },
+  {
+    title: "JDN9505 - BLUE DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain8/chain1.jpg'),
+    imageTwo: require('./chainImg/chain8/chain2.jpg')
+  },
+  {
+    title: "JDN40206 - DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain9/chain1.jpg'),
+    imageTwo: require('./chainImg/chain9/chain2.jpg')
+  },
+  {
+    title: "N15116 - DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain10/chain1.jpg'),
+    imageTwo: require('./chainImg/chain10/chain2.jpg')
+  },
+  {
+    title: "N170530 - LADY DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain11/chain1.jpg'),
+    imageTwo: require('./chainImg/chain11/chain2.jpg')
+  },
+  {
+    title: "N170703 - DIAMOND CHAIN",
+    price: "17,000",
+    imageOne: require('./chainImg/chain12/chain1.jpg'),
+    imageTwo: require('./chainImg/chain12/chain2.jpg')
+  },
+
+
+];
